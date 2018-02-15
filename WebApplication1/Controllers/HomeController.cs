81d41e9e10aeb2dbfcc3820b3280a4371fcc2042
manojcisin <manoj.s@cisinlabs.com>
2018-02-15 18:35:55 +0000

    Update HomeController.cs
    Changes from Manoj

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -29,7 +29,7 @@ namespace WebApplication1.Controllers
 
         public ActionResult Login()
         {
-            ViewBag.SuccessMessage = "This is a login Screen";
+            ViewBag.SuccessMessage = "Manoj Changed the Message";
             return View();
         }
     }
